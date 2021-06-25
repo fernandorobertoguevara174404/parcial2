bfa48c87f2a49435d7b92128b78ce43ae6351aff
fernandorobertoguevara174404 <a174404@une.edu.mx>
2021-06-24 22:56:07 -0500

    Nota3

--- config.py
+++ config.py
@@ -3,6 +3,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "DASJE#$FGQP)5644F"
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI") or \
-        "sqlite:///" + os.path.join(basedir,"app.db")
+    if os.getenv("DATABASE_URL"):
+        SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL").replace("postgres", "postgresql")
+    else:
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    print(SQLALCHEMY_DATABASE_URI)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
